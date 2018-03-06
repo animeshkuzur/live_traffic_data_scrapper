b6a35a7fef089bf9dd2a3bac68c2d3a991922dfb
animeshkuzur <animesh.kuzur@outlook.com>
2018-03-06 16:00:48 +0530

    log inculded

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ from decimal import Decimal, getcontext
 
 from config import TOP_LAT, TOP_LOG, BOTTOM_LAT, BOTTOM_LOG
 
+def logger(text):
+	file = open('log', 'a')
+	file.write("["+time.strftime("%H:%M:%S")+"]"+text+"\n")
+	print(text)	
+	file.close()
+
 class Coordinates(object):
 	def __init__(self,lat,log):
 		self._lat = lat
@@ -41,11 +47,11 @@ class Crawl(Coordinates):
 	def scrapper(self):
 		while True:
 			time.sleep(240)
-			print("refeshing browser:"+self._c)
+			logger("refeshing browser:"+self._c)
 			self._browser.refresh()
 			time.sleep(60)
 			t = time.strftime("%H:%M:%S")
-			print("window "+self._c+" screenshot : "+t)
+			logger("window "+self._c+" screenshot : "+t)
 			self._browser.save_screenshot("./data/"+self._c+"/"+t+".png")
 
 	def run(self):
@@ -55,7 +61,7 @@ class Crawl(Coordinates):
 		self._bottom=self._browser.find_element_by_id("bl").text
 		self._left=self._browser.find_element_by_id("ll").text
 		self._right=self._browser.find_element_by_id("rl").text
-		print("Creating description file...")
+		logger("Creating description file...")
 		file = open("./data/"+self._c+"/DESC","w")
 		file.write(self._top+"\n")
 		file.write(self._bottom+"\n") 
@@ -63,7 +69,7 @@ class Crawl(Coordinates):
 		file.write(self._right+"\n")
 		file.close()
 
-		print("Creating a new Thread...")
+		logger("Creating a new Thread...")
 		self._thread_dict[self._c]=Thread(target=self.scrapper, args=())
 		self._thread_dict[self._c].start()
 
@@ -84,9 +90,10 @@ class App(Coordinates):
 		return flag
 
 	def callibrate(self):
-		print("\nInitiating Callibration")
+		logger("Initiating Callibration")
 		if not self.check_dir("data"):
 			os.makedirs("data")
+
 		browser = webdriver.Firefox()
 		browser.maximize_window()
 		browser.get(tl.get_url())
@@ -95,9 +102,9 @@ class App(Coordinates):
 		left=browser.find_element_by_id("ll").text
 		right=browser.find_element_by_id("rl").text
 		browser.quit()
-		print(top,bottom,left,right)
+		logger(top+" "+bottom+" "+left+" "+right)
 		map_parts = self.compute(top,bottom,left,right)
-		print(str(map_parts)+" total windows")
+		logger(str(map_parts)+" total windows")
 
 		file = open("./data/DESC","w")
 		file.write(TOP_LAT+"\n")
@@ -112,7 +119,7 @@ class App(Coordinates):
 		getcontext().prec = 6
 		lat_diff1 = abs(Decimal(top)-Decimal(tl.get_lat()))
 		lat_diff2 = abs(Decimal(bottom)-Decimal(tl.get_lat()))
-		print(lat_diff1,lat_diff2)
+		logger(str(lat_diff1)+" "+str(lat_diff2))
 		lat_diff = min(lat_diff1,lat_diff2)
 		a=Decimal(tl.get_lat())
 		self._lat_diff=lat_diff;
@@ -120,11 +127,11 @@ class App(Coordinates):
 		while(a>Decimal(bl.get_lat())):
 			a=a-Decimal(lat_diff*2)
 			x=x+1
-		print(str(x)+" rows windows")
+		logger(str(x)+" rows windows")
 		
 		log_diff1 = abs(Decimal(left)-Decimal(tl.get_log()))
 		log_diff2 = abs(Decimal(right)-Decimal(tl.get_log()))
-		print(log_diff1,log_diff2)
+		logger(str(log_diff1)+" "+str(log_diff2))
 		log_diff = min(log_diff1,log_diff2)
 		b=Decimal(tl.get_log())
 		y=0
@@ -132,7 +139,7 @@ class App(Coordinates):
 		while(b<Decimal(tr.get_log())):
 			b=b+Decimal(log_diff*2)
 			y=y+1
-		print(str(y)+" column windows")
+		logger(str(y)+" column windows")
 
 		return x*y
 
@@ -145,38 +152,39 @@ class App(Coordinates):
 			temp_long=Decimal(tl.get_log())
 			while(temp_long<=Decimal(tr.get_log())):
 				c+=1
-				print("\nProcessing Block Number: ",c)
-				print("------------------------")
+				logger("Processing Block Number: "+str(c))
+				logger("------------------------")
 				if not self.check_dir("data/"+str(c)):
 					os.makedirs("data/"+str(c))
 				temp_long=temp_long+(self._log_diff*2)
 				dict[c]=Coordinates(str(temp_lat),str(temp_long))
-				print("creating object ",c)
+				logger("creating object "+str(c))
 				Crawl(dict[c],c).run()
 			temp_lat=temp_lat-(self._lat_diff*2)
 			break #temp
 		return 0
 
-print("checking previous cache data")
+
+logger("checking previous cache data")
 if os.path.exists("data"):
-	print("clearing cache data\n")
+	logger("clearing cache data\n")
 	shutil.rmtree("data", ignore_errors=True)
 
-print("Co-ordinates")
-print("\n-----Top left-----")
-print(TOP_LAT,TOP_LOG)
+logger("Co-ordinates")
+logger("-----Top left-----")
+logger(TOP_LAT+" "+TOP_LOG)
 tl=Coordinates(TOP_LAT,TOP_LOG)
 
-print("\n-----Top right-----")
-print(TOP_LAT,BOTTOM_LOG)
+logger("-----Top right-----")
+logger(TOP_LAT+" "+BOTTOM_LOG)
 tr=Coordinates(TOP_LAT,BOTTOM_LOG)
 
-print("\n-----Bottom right-----")
-print(BOTTOM_LAT,BOTTOM_LOG)
+logger("-----Bottom right-----")
+logger(BOTTOM_LAT+" "+BOTTOM_LOG)
 br=Coordinates(BOTTOM_LAT,BOTTOM_LOG)
 
-print("\n-----Bottom left-----")
-print(BOTTOM_LAT,TOP_LOG)
+logger("-----Bottom left-----")
+logger(BOTTOM_LAT+" "+TOP_LOG)
 bl=Coordinates(BOTTOM_LAT,TOP_LOG)
 
 x=App(tl,tr,br,bl)
